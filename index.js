ec43feae908cac0813b77edd60cc8a372728e022
hinanshisuthar <hinanshis4@gmail.com>
2022-02-27 01:10:22 +0530

    map

--- index.js
+++ index.js
@@ -36,3 +36,21 @@ import './style.css';
 
 // const hinanshiAfterBirthday = birthday(hinanshi);
 // console.log(hinanshiAfterBirthday);
+
+//array-filter
+
+// const words = ['hello', 'hey', 'coocoo', 'bonjour'];
+// console.log(words.filter((word) => word.length > 4));
+
+// const numbers = [1, 4, 20, 83, 35, 14];
+// const checkNumber = (num) => num > 10;
+
+// console.log(numbers.filter(checkNumber));
+
+// console.log(numbers.map((num) => num + 2));
+
+// console.log(
+//   numbers.map((num) => {
+//     num;
+//   })
+// );
