014e88657211ded7ccc1a98e2fffcfc442d4fe24
hinanshisuthar <hinanshis4@gmail.com>
2022-02-26 23:17:29 +0530

    pure functions

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// 1
+
+// const hinanshi = {
+//   name: 'rekha',
+//   age: 20,
+// };
+
+// const devanshi = {
+//   ...hinanshi,
+//   age: hinanshi.age + 2,
+// };
+
+// console.log(devanshi);
+// console.log(hinanshi === devanshi);
+
+//2
+
+// let arrayOfColors = ['red', 'pink', 'green', 'yellow', 'blue'];
+
+// let NewArrayOfColors = [...arrayOfColors, 'black', 'grey'];
+// console.log(NewArrayOfColors);
+
+// console.log(arrayOfColors === NewArrayOfColors);
+
+//3
+
+// const birthday = (hinanshi) => ({ ...hinanshi, age: hinanshi.age + 1 });
+
+// const hinanshi = {
+//   name: 'hinanshi',
+//   age: 20,
+// };
+
+// const hinanshiAfterBirthday = birthday(hinanshi);
+// console.log(hinanshiAfterBirthday);
